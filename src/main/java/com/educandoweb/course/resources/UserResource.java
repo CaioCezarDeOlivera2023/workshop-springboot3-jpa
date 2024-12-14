e84686f48b05cc292721c1f198430f679ca3e5d7
Caio <caio_25m@hotmail.com>
2024-12-14 13:00:53 -0300

    H2 database

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -15,7 +15,7 @@ public class UserResource {
 	@GetMapping
 	public ResponseEntity<User> findAll(){
 		
-		User u= new User(1L, "Maria", "maria@gmail.com", "123456789", "12345");
+		User u = new User(1L, "Maria", "maria@gmail.com", "123456789", "12345");
 		return ResponseEntity.ok().body(u);
 	}
 }
