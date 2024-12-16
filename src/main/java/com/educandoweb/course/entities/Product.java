db1dd451462dc8a887602738bf37583d7c2eba47
Caio <caio_25m@hotmail.com>
2024-12-16 16:02:55 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -8,15 +8,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable  {
-	
+public class Product implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -24,18 +26,20 @@ public class Product implements Serializable  {
 	private String description;
 	private Double price;
 	private String imgUrl;
-	
-	/* Usando Set, para garantir que não tenha a mesma categori de produto
+
+	/*
+	 * Usando Set, para garantir que não tenha a mesma categori de produto
 	 * 
 	 */
-	@Transient
-	private Set<Category> categories = new HashSet<>(); /*aqui esta garantindo que a coleção category 
-														nao comece com valor nulo, e sim vazio*/
-	
-	
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>(); /*
+														 * aqui esta garantindo que a coleção category nao comece com
+														 * valor nulo, e sim vazio
+														 */
+
 	public Product() {
 	}
-
 
 	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
@@ -46,61 +50,49 @@ public class Product implements Serializable  {
 		this.imgUrl = imgUrl;
 	}
 
-
 	public Long getId() {
 		return id;
 	}
-
 
 	public void setId(Long id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-
 	public String getDescription() {
 		return description;
 	}
-
 
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
-
 	public Double getPrice() {
 		return price;
 	}
-
 
 	public void setPrice(Double price) {
 		this.price = price;
 	}
 
-
 	public String getImgUrl() {
 		return imgUrl;
 	}
-
 
 	public void setImgUrl(String imgUrl) {
 		this.imgUrl = imgUrl;
 	}
 
-
 	public Set<Category> getCategories() {
 		return categories;
 	}
-
 
 	@Override
 	public int hashCode() {
@@ -109,7 +101,6 @@ public class Product implements Serializable  {
 		result = prime * result + ((description == null) ? 0 : description.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
